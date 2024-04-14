eb7f66973bf4c5a48b77f10693e6a7f6a46d14a2
abeerhassan2002 <167028263+abeerhassan2002@users.noreply.github.com>
2024-04-14 21:23:04 +0200

    Add files via upload

---
+++ test_heart.py
@@ -0,0 +1,22 @@
+import pickle
+import numpy as np
+
+# loading the saved model
+loaded_model = pickle.load(open(r"C:\Users\HP\Documents\heart_disease_model.sav", 'rb'))
+
+
+input_data = (62,0,0,140,268,0,0,160,0,3.6,0,2,2)
+
+# change the input data to a numpy array
+input_data_as_numpy_array= np.asarray(input_data)
+
+# reshape the numpy array as we are predicting for only on instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0]== 0):
+  print('The Person does not have a Heart Disease')
+else:
+  print('The Person has Heart Disease')
